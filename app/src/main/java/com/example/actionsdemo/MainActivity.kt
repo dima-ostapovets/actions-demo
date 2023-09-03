fddc6985bf1ece94ba073041a98de9eed3c17793
Dmytro Ostapovets <dmytro.ostapovets@gen.tech>
2023-09-03 21:39:54 +0300

    trigger build

--- app/src/main/java/com/example/actionsdemo/MainActivity.kt
+++ app/src/main/java/com/example/actionsdemo/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.actionsdemo.ui.theme.ActionsDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ActionsDemoTheme {
                 // A surface container using the 'background' color from the theme
